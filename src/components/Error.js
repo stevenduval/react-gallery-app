a04f2340f4a03f9446b470673cd39492dabafcca
stevenduval <stevenduval@gmail.com>
2020-06-23 12:46:55 -0500

    updated image src path

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ import React from 'react';
 // return the following error message when component is called
 const Error = (props) => (
     <div className="container">
-        <img src="%PUBLIC_URL%/sad-businessman.png" alt="Sad Businessman" height="200" />
+        <img src={process.env.PUBLIC_URL + '/sad-businessman.png'} alt="Sad Businessman" height="200" />
         <br />
         <br />
         <h2>Page Not Found!</h2>
